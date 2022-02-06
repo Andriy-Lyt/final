bdf8579b79fb1fc7016b9320ce9213366b1343ca
Andriy-Lyt <howdy_eh@icloud.com>
2022-02-06 12:24:14 -0500

    moved client code to client folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Lets Rock the Final!
+     <span style={{fontSize:"30px"}}> &#128512; </span>
+     Yeah!
+     </h2>
+    </div>
+  );
+}
+
+export default App;
